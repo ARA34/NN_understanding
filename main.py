22b3110bd39a9a19ddba146d213cda2af8529f1e
Alex Reyes Aranda <agreyesaranda@gmail.com>
2023-08-21 19:23:18 -0700

    updated main

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Layer_Dense:
         self.weights = 0.10 * np.random.randn(n_inputs,n_neurons)
         self.biases = np.zeros((1,n_neurons))
     def forward(self, inputs):
-        self.output = np.maximum(0,inputs)
+        self.output = np.dot(inputs, self.weights) + self.biases
 
 class Activation_ReLU:
     def forward(self, inputs):
